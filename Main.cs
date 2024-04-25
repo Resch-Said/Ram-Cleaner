4342f81f3eb44234a71c47fb37b7421b1d844bad
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-04-25 07:28:08 +0200

    Add memory cleaner

---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ram_Cleaner
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            int cleanerThreshold = 0;
+
+            try
+            {
+                Console.WriteLine("Enter the threshold in percentage for the cleaner to run: ");
+                cleanerThreshold = int.Parse(Console.ReadLine());
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            while (true)
+            {
+                try
+                {
+                    if (ProcessUtil.GetUsedRAMPercentage() > cleanerThreshold)
+                    {
+                        Console.WriteLine("Cleaning RAM...");
+                        Process[] processes = ProcessUtil.GetAllProccesses();
+                        foreach (Process process in processes)
+                        {
+                            try
+                            {
+                                if (!process.HasExited)
+                                {
+                                    ProcessUtil.EmptyWorkingSet(process);
+                                    Console.WriteLine("Cleaned: " + process.ProcessName);
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+                Console.WriteLine("Used RAM: " + ProcessUtil.GetUsedRAMPercentage() + "%");
+                System.Threading.Thread.Sleep(5000); // Sleep for 5 seconds
+            }
+        }
+    }
+}
---
+++ Process.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ram_Cleaner
+{
+    class ProcessUtil
+    {
+
+        public static Process[] GetAllProccesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            return processes;
+
+        }
+
+        public static void EmptyWorkingSet(Process process)
+        {
+            [DllImport("psapi.dll")]
+            static extern int EmptyWorkingSet(IntPtr hwProc);
+            EmptyWorkingSet(process.Handle);
+        }
+
+        public static double GetRAMUsage(Process process)
+        {
+            return (double)process.WorkingSet64 * 1e-09d;
+        }
+
+        public static double GetTotalRAMUsage()
+        {
+            double totalRAM = 0;
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                totalRAM += GetRAMUsage(process);
+            }
+            return totalRAM;
+        }
+
+        public static float GetSystemRAM()
+        {
+            return GC.GetGCMemoryInfo().TotalAvailableMemoryBytes * 1e-09f;
+        }
+
+        public static double GetAvailableRAM()
+        {
+            return GetSystemRAM() - GetTotalRAMUsage();
+        }
+
+        public static double GetUsedRAMPercentage()
+        {
+            return Math.Round((GetTotalRAMUsage() / GetSystemRAM()) * 100, 2);
+        }
+
+    }
+}
